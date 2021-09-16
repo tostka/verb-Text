e3360d8c1dfd1e4cb32674b2fc1cef4e715ea0f5
Tostka <tostka@users.noreply.github.com>
2021-09-16 10:48:52 -0500

    Update convertFrom-Base64String.ps1
    * 10:38 AM 9/16/2021 removed spurious DefaultParameterSet matl and file/path test material (from convertto-b64...), added pipeline example to CBH, fixed CBH params (had convertfrom params spec'd); added email address conversion example

--- Public/convertFrom-Base64String.ps1
+++ Public/convertFrom-Base64String.ps1
@@ -17,7 +17,7 @@ function convertFrom-Base64String {
     AddedWebsite:	URL
     AddedTwitter:	URL
     REVISIONS
-    * 10:38 AM 9/16/2021 removed spurious DefaultParameterSet matl and file/path test material (from convertto-b64...), added pipeline example to CBH
+    * 10:38 AM 9/16/2021 removed spurious DefaultParameterSet matl and file/path test material (from convertto-b64...), added pipeline example to CBH, fixed CBH params (had convertfrom params spec'd); added email address conversion example
     * 8:26 AM 12/13/2019 convertFrom-Base64String:init
     .DESCRIPTION
     convertFrom-Base64String - Convert specified string from Base64 encoded string back to text and return to pipeline
@@ -27,8 +27,8 @@ function convertFrom-Base64String {
     PS> convertFrom-Base64String -string 'bXkgKnZlcnkqIG1pbmltYWxseSBvYmZ1c2NhdGVkIGluZm8=' ; 
     Convert Base64 encoded string back to original unencoded text
     .EXAMPLE
-    PS> 'bXkgKnZlcnkqIG1pbmltYWxseSBvYmZ1c2NhdGVkIGluZm8=' | convertTo-Base64String
-    Pipeline conversion of an string address from Base64.
+    PS> $EmailAddress = 'YWRkcmVzc0Bkb21haW4uY29t' | convertFrom-Base64String
+    Pipeline conversion of encoded EmailAddress back to string.
     .LINK
     #>
     [CmdletBinding()]
