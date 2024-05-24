73d00cb4488f80ac8d088a1db2c1460c97f17963
Tostka <tostka@users.noreply.github.com>
2024-05-24 13:01:47 -0500

    Update convertTo-Base64String.ps1
    * 10:12 AM 5/23/2024 add: -ForceString param, to permit encoding paths to b64 (insted of attempting to convert the file contents to b64).

--- Public/convertTo-Base64String.ps1
+++ Public/convertTo-Base64String.ps1
@@ -17,16 +17,21 @@ function convertTo-Base64String {
     AddedWebsite:	URL
     AddedTwitter:	URL
     REVISIONS
+    * 10:12 AM 5/23/2024 add: -ForceString param, to permit encoding paths to b64 (insted of attempting to convert the file contents to b64).
     * 11:02 AM 9/5/2023 updated catch: wasn't echo'ing anything, just throw everything out.
     * 3:58 PM 4/20/2022 Work in progress, was adding file 'sourcefile/targetfile' support, untested, I believe I had issues with the conversion, b64 wouldn't convert cleanly back to original (as part of the encoding for the invoke-soundcue bundled ping updates).
     * 10:27 AM 9/16/2021 updated CBH, set -string as position 0, flipped pipeline to string from path, removed typo $file test, pre-resolve-path any string, and if it resolves to a file, load the file for conversion. Shift path validation into the body. 
     * 8:26 AM 12/13/2019 convertTo-Base64String:init
     .DESCRIPTION
     convertTo-Base64String - Convert specified string or Path-to-file to Base64 encoded string and return to pipeline. If String resolves to a path, it will be treated as a -path parameter (file content converted to Base64 encoded string). 
-    .PARAMETER  SourceFile
-    File to be Base64 encoded (image, text, whatever)[-SourceFile path-to-file]
-    .PARAMETER  string
-    String to be Base64 encoded [-string 'string to be encoded']
+    .PARAMETER string
+    String to be decoded from Base64 [-string 'bXkgKnZlcnkqIG1pbmltYWxseSBvYmZ1c2NhdGVkIGluZm8']
+    .PARAMETER SourceFile
+    Optional param that designates path from which to read a file containing Base64 encoded content, to be decoded[-SourceFile 'c:\path-to\base64.txt']
+    .PARAMETER TargetFile
+    Optional param that designates path into which to write the decoded Base64 content [-TargetPath 'c:\path-to\file.png']
+    .PARAMETER ForceString
+    Optional param that forces treament of -String as a string (vs file; avoids mis-recognition of string as a path to a file to be converted)
     .EXAMPLE
     PS> convertTo-Base64String -SourceFile C:\Path\To\Image.png > base64.txt ; 
     Example converting a png file to base64 and outputing result to text using redirection
@@ -43,15 +48,17 @@ function convertTo-Base64String {
     [CmdletBinding(DefaultParameterSetName='File')]
     PARAM(
         [Parameter(Position=0,ValueFromPipeline=$true,HelpMessage="string to be decoded from Base64 [-string 'bXkgKnZlcnkqIG1pbmltYWxseSBvYmZ1c2NhdGVkIGluZm8']")]
-        [String]$string,
+            [String]$string,
         [Parameter(HelpMessage="Optional param that designates path from which to read a file to be Base64 encoded[-SourceFile 'c:\path-to\base64.txt']")]
-        [string]$SourceFile,
+            [string]$SourceFile,
         [Parameter(HelpMessage="Optional param that designates path into which to write the encoded Base64 content [-TargetPath 'c:\path-to\file.png']")]
-        [string]$TargetFile
+            [string]$TargetFile,
+        [Parameter(HelpMessage="Optional param that forces treament of -String as a string (vs file; avoids mis-recognition of string as a path to a file to be converted)")]
+            [switch]$ForceString
     ) ;
     $error.clear() ;
     TRY {
-        if($SourceFile -OR ($SourceFile = $string| Resolve-Path -ea 0)){
+        if($SourceFile -OR ( -not $ForceString -AND ($SourceFile = $string| Resolve-Path -ea 0) ) ){
             if(test-path $SourceFile){
                 write-verbose "(loading specified/resolved SourceFile:$($SourceFile))" ; 
                 <# Get-Content without -raw splits the file into an array of lines thus destroying the code
